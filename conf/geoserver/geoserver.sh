d836712e97b0af150aea518667f73bec23015060
Ken Lippold <kjlippold@icloud.com>
2020-11-22 22:24:21 -0500

    Statis updates

--- conf/geoserver/geoserver.sh
+++ conf/geoserver/geoserver.sh
@@ -9,13 +9,13 @@ addgroup --gid 1099 tomcat && useradd -m -u 1099 -g tomcat tomcat \
 if [ -n "${CUSTOM_UID}" ];then
   echo "Using custom UID ${CUSTOM_UID}."
   usermod -u ${CUSTOM_UID} tomcat
-  find / -user 1099 -exec chown -h tomcat {} \; 
+  find / -path /projects -prune -false -o -user 1099 -exec chown -h tomcat {} \; 
 fi
 
 if [ -n "${CUSTOM_GID}" ];then
   echo "Using custom GID ${CUSTOM_GID}."
   groupmod -g ${CUSTOM_GID} tomcat
-  find / -group 1099 -exec chgrp -h tomcat {} \;
+  find / -path /projects -prune -false -o -group 1099 -exec chgrp -h tomcat {} \;
 fi
 
 # Allow Anonymous GET Access
